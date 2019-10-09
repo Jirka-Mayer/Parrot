0330e8a35a5e9d574dd2db229e87d3ce3b059705
Jirka-Mayer <jirkamayer97@gmail.com>
2019-10-09 09:41:07 +0200

    v0.1.0

--- Parrot/Properties/AssemblyInfo.cs
+++ Parrot/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo("ParrotTests")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-dev")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
